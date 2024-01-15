13435a71c35c3b2980e85d5d2407c8f874b93971
fathursyh <fathur.syah1@gmail.com>
2024-01-15 16:30:11 +0700

    update custome widget

---
+++ lib/custom_widget/gradient_container.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(
+      {super.key,
+      required this.warna1,
+      required this.warna2,
+      this.warna3,
+      required this.child});
+
+  final Color warna1;
+  final Color warna2;
+  final Color? warna3;
+  final Widget child;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: (warna3 == null
+                  ? [warna1, warna2]
+                  : [warna1, warna2, warna3!]),
+              begin: Alignment.topCenter,
+              stops: (warna3 == null ? [0, 0.6] : [0, 0.6, 1]),
+              end: Alignment.bottomCenter)),
+      child: Center(
+        child: child,
+      ),
+    );
+  }
+}
